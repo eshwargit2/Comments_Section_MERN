5ba19ebb6f4b75e276c17bdf1d4c13a3c5e00f65
eshwargit2 <msselectronic57@gmail.com>
2025-07-19 18:39:31 +0530

    updated

--- frontend/src/Commant.jsx
+++ frontend/src/Commant.jsx
@@ -33,28 +33,6 @@ const handleSubmit = async (e) => {
 
   return (
     <div className='Sign-section w-300'>
-        {/* <div className="container">
-            <div className="row">
-                <div className="col-12 text-center mt-3">
-                    <h2>Update Delivery Address</h2>
-                </div>
-                 <div className="col-12 mt-5 pb-5" >
-                  <form   onSubmit={handleSubmit} > 
-                    <input type="text" name='country' onChange={handleChange} placeholder='Country/Region' required />
-                    <input type="name" name='name'   onChange={handleChange} placeholder='Full name'  required />
-                    <input type="number" name='number'  onChange={handleChange}  placeholder='mobile number'  required />
-                 <input type="text" name='address1'   onChange={handleChange} placeholder='Flat,House no.,Building , Company, Apartment'  required />
-                    <input type="text" name='address2'  onChange={handleChange}  placeholder='Area, Street, Sector, Village'  required />
-                    <input type="text" name='address3'  onChange={handleChange}  placeholder='Landmark'  required />
-                    <input type="text" name='pincode'   onChange={handleChange} placeholder='pincode'  required />
-                    <input type="text" name='city'   onChange={handleChange} placeholder='Town/City'  required />
-                    <input type="text" name='state'  onChange={handleChange}  placeholder='State'  required />       
-                       <button type='submit' id='submit' className='mt-3 '> Use this Address </button>
-                    </form>
-                </div>
-            </div>  
-        </div> */}
-
         <section class="w-full rounded-lg border-2 border-black p-4 my-8 mx-auto max-w-xl">
     <h3 class="font-os text-lg font-bold text-black inline"><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#000000"><path d="M240-400h480v-66.67H240V-400Zm0-126.67h480v-66.66H240v66.66Zm0-126.66h480V-720H240v66.67ZM880-80 720-240H146.67q-27 0-46.84-19.83Q80-279.67 80-306.67v-506.66q0-27 19.83-46.84Q119.67-880 146.67-880h666.66q27 0 46.84 19.83Q880-840.33 880-813.33V-80ZM146.67-306.67h602l64.66 68.34v-575H146.67v506.66Zm0 0v-506.66 506.66Z" className='inline'/></svg>Comments</h3>
 
